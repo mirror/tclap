83ff0a8ccaf26a8e384c010645e64037b62fc9b4
mes5k <mes5k>
2003-12-22 02:10:26 +0000

    updated

--- tests/test14.sh
+++ tests/test14.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-../examples/test3  -s aaa homer marge bart -- one two > tmp.out 2>&1
+../examples/test3  -s=aaa homer marge bart -- one two > tmp.out 2>&1
 
 if cmp -s tmp.out test14.out; then
 	exit 0
