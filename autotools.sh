5683b590ce9ddcf83c8d6e584e2ee085ff4705f2
mes5k <mes5k>
2004-04-26 15:18:08 +0000

    fixed for autotools for mandrake

--- autotools.sh
+++ autotools.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-aclocal -I config  
-autoconf
-autoheader
-automake -a
+echo "Using autotools versions:"	
+if [[ -e autoconf-2.5x && -e automake-1.7 ]]
+then
+	autoconf-2.5x --version | grep autoconf
+	automake-1.7 --version | grep automake
+	aclocal-1.7 -I config  
+	autoconf-2.5x
+	autoheader-2.5x
+	automake-1.7 -a
+else
+	autoconf --version | grep autoconf
+	automake --version | grep automake
+	aclocal -I config  
+	autoconf
+	autoheader
+	automake -a
+fi
