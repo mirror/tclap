61ddf68d75a807ddbb1a6887bbcbf71e110d0c87
macbishop <macbishop>
2004-09-26 14:58:06 +0000

    Runs all tests and sumarizes the result

---
+++ tests/runtests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+let "suc = 0"
+let "fail = 0"
+
+for tnam in `ls test??.sh`
+  do 
+  echo "Running test $tnam"
+  if ./$tnam && echo "OK" || echo "FAIL" ; then
+      let "suc = $(($suc + 1))"
+  else
+      let "fail = $(($fail + 1))"
+  fi
+done
+
+let "total = $(($suc + $fail))"
+echo "======================"
+echo "|     TESTS DONE     |"
+echo "======================"
+echo " TOTAL: $total"
+echo "    OK: $suc"
+echo "FAILED: $fail"
+
