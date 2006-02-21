e5a757999f20eb1964d96c2ef32abf875fa230f4
zeekec <zeekec>
2006-02-21 11:05:15 +0000

    Added virtual destructor to silence warnings.

--- include/tclap/Constraint.h
+++ include/tclap/Constraint.h
@@ -1,23 +1,23 @@
 
-/****************************************************************************** 
- * 
+/******************************************************************************
+ *
  *  file:  Constraint.h
- * 
+ *
  *  Copyright (c) 2005, Michael E. Smoot
  *  All rights reverved.
- * 
+ *
  *  See the file COPYING in the top directory of this distribution for
  *  more information.
- *  
- *  THE SOFTWARE IS PROVIDED _AS IS_, WITHOUT WARRANTY OF ANY KIND, EXPRESS 
- *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
- *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- *  DEALINGS IN THE SOFTWARE.  
- *  
- *****************************************************************************/ 
+ *
+ *  THE SOFTWARE IS PROVIDED _AS IS_, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ *  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
 
 #ifndef TCLAP_CONSTRAINT_H
 #define TCLAP_CONSTRAINT_H
@@ -32,15 +32,15 @@
 namespace TCLAP {
 
 /**
- * The interface that defines the interaction between the Arg and Constraint. 
+ * The interface that defines the interaction between the Arg and Constraint.
  */
 template<class T>
-class Constraint 
+class Constraint
 {
 
 	public:
 		/**
-		 * Returns a description of the Constraint. 
+		 * Returns a description of the Constraint.
 		 */
 		virtual std::string description() const =0;
 
@@ -52,11 +52,17 @@ class Constraint
 		/**
 		 * The method used to verify that the value parsed from the command
 		 * line meets the constraint.
-		 * \param value - The value that will be checked. 
+		 * \param value - The value that will be checked.
 		 */
 		virtual bool check(const T& value) const =0;
 
+		/**
+		 * Destructor.
+		 * Silences warnings about Constraint being a base class with virtual
+		 * functions but without a virtual destructor.
+		 */
+		virtual ~Constraint() { ; }
 };
 
 } //namespace TCLAP
-#endif 
+#endif
