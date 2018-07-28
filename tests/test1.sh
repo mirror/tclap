6db23c03b5dbc5ba60998527c1cd62f13466a4c7
Daniel Aarno <aarno@google.com>
2018-07-28 18:09:56 +0100

    Break out some common test code to a separate file.
    Next step to refactor remaining tests.

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-# success
-../examples/test1 -r -n mike > tmp.out 2>&1
-
-if cmp -s tmp.out $srcdir/test1.out; then
-	exit 0
-else
-	exit 1
-fi
-
+./simple-test.sh test1 `basename $0 .sh` -r -n mike
