4146640e254058a47850b768f32b75d8fb258743
Daniel Aarno <aarno@google.com>
2019-11-17 12:25:20 +0000

    Add script to run and sumarize tests
    Keeps a list of known failures and reports only on changes. This makes it easier to go through all now failing tests and fix them

---
+++ tests/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import glob
+import subprocess
+from typing import Sequence
+
+def test(tests: Sequence[str], expected_fail: Sequence[str]) -> None:
+    expected_fail = frozenset(t.strip() for t in expected_fail)
+    passing = 0
+    failing = 0
+    for t in tests:
+        r = subprocess.run(['./' + t],
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL)
+        if r.returncode == 0:
+            passing += 1
+            if t in expected_fail:
+                print('Unexpected PASS: ' + t)
+        else:
+            failing += 1
+            if t not in expected_fail:
+                print('Unexpected FAIL: ' + t)
+    print('PASS: %d / FAIL: %d' % (passing, failing))
+
+def build():
+    subprocess.run(['make', '-C', '../examples', '-j', '8'])
+
+def main():
+    build()
+    with open('expected-failures.txt') as expected_fail:
+        test(glob.glob('test*.sh'), expected_fail.readlines())
+
+if __name__ == '__main__':
+    main()
