c398dd3d7d4c7a1e9c857309372820f3ff880c15
Daniel Aarno <aarno@google.com>
2020-03-26 10:04:47 +0000

    Display CI build status on sourceforge

--- docs/upload.sh
+++ docs/upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rsync -aP html index.html manual.html style.css \
+rsync -aP html index.html manual.html style.css build.html \
       $USER@web.sourceforge.net:/home/project-web/tclap/htdocs
