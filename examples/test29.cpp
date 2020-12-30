93ee0c81c14351307595887a4f2f7398b4dc6e54
Daniel Aarno <aarno@google.com>
2020-12-30 13:05:20 +0000

    Remove unused arguments from test (fix warnings).

--- examples/test29.cpp
+++ examples/test29.cpp
@@ -6,7 +6,7 @@ using namespace TCLAP;
 using namespace std;
 
 // https://sourceforge.net/p/tclap/bugs/30/
-int main(int argc, char** argv) {
+int main() {
   CmdLine cmd("test empty argv");
   std::vector<string> args;
   cmd.parse(args);
