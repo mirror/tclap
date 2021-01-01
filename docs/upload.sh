dd1a4670bf43427b1a378a99c8f5a3948f9cec67
Daniel Aarno <aarno@google.com>
2021-01-01 16:23:19 +0000

    Start putting docs under "v1.2".
    Preparation for release of v1.4.

--- docs/upload.sh
+++ docs/upload.sh
@@ -3,4 +3,4 @@
 set -x
 
 rsync -aP html index.html manual.html style.css build.html \
-      $USER@web.sourceforge.net:/home/project-web/tclap/htdocs
+      $USER@web.sourceforge.net:/home/project-web/tclap/htdocs/v1.2
