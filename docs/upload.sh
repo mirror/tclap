a5e41ae17e05ffcab80eebff52b252f06a343162
Daniel Aarno <aarno@google.com>
2021-01-05 22:02:35 +0000

    Upload docs for 1.4 to both 1.4 and root.

--- docs/upload.sh
+++ docs/upload.sh
@@ -4,3 +4,6 @@ set -x
 
 rsync -aP html index.html manual.html build.html style.css \
       $USER@web.sourceforge.net:/home/project-web/tclap/htdocs/v1.4
+
+rsync -aP html index.html manual.html build.html style.css \
+      $USER@web.sourceforge.net:/home/project-web/tclap/htdocs/
