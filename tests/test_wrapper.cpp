f4582f7370fbc32de7d6b1c21f840bac03a43795
Daniel Aarno <aarno@google.com>
2021-03-29 22:06:25 +0200

    Clean up return branches, no need to keep a result variable.
    Makes code more localized, and easier to read.

--- tests/test_wrapper.cpp
+++ tests/test_wrapper.cpp
@@ -4,9 +4,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <iterator>
 #include <string>
-#include <iostream>
 
 class TmpFile {
 public:
@@ -37,7 +37,6 @@ private:
  * value indicating the type of error.
  */
 int main(int argc, char *argv[]) {
-  int result = -1;  // Unknown failure
   try {
     TCLAP::CmdLine argParser("Runs a command with given parameters and"
                              " compares its output to stdout with the "
@@ -63,7 +62,8 @@ int main(int argc, char *argv[]) {
 
     /* Compare against contents of reference file. */
     std::ifstream &f_tmp = tmpFile.stream();
-    std::ifstream f_ref(refFile.getValue(), std::ifstream::binary | std::ifstream::ate);
+    std::ifstream f_ref(refFile.getValue(),
+                        std::ifstream::binary | std::ifstream::ate);
     if (f_tmp.fail() || f_ref.fail()) {
       return 2; // One of the files fail.
     }
@@ -75,11 +75,10 @@ int main(int argc, char *argv[]) {
     bool bEqual = std::equal(std::istreambuf_iterator<char>(f_tmp.rdbuf()),
                              std::istreambuf_iterator<char>(),
                              std::istreambuf_iterator<char>(f_ref.rdbuf()));
-    result = bEqual ? 0 : 4; // 4 = Files to not compare equal.
+    return bEqual ? 0 : 4; // 4 = Files to not compare equal.
   } catch (TCLAP::ArgException &e) {
     std::cerr << e.what() << std::endl;
-    result = -2;
   }
 
-  return result;
+  return -1;
 }
